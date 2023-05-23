4e843414fa5e743338018f890a7b5f0b07967e80
Brenda Guardines <82037465+brenguardines@users.noreply.github.com>
2023-05-23 15:30:53 -0300

    modificacion en test

--- src/test/java/org/example/PrendaTest.java
+++ src/test/java/org/example/PrendaTest.java
@@ -2,29 +2,48 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class PrendaTest {
-
-
   @Test
-  void unaRemeraDeAlgodonAzul() {
-    TipoPrenda tipo = new TipoPrenda("Remera", Categoria.PARTE_SUPERIOR);
-    Material material = Material.ALGODON;
-    ColorPrimario colorPrimario = new ColorPrimario(0,0,255);
+  void unaPrendaSinColorSecundario() {
+    assertEquals(unaRemeraDeAlgodonAzul().getColorSecundario(), null);
   }
 
   @Test
-  void unPantalonDeJeanBlancoYRojo() {
-    TipoPrenda tipo = new TipoPrenda("Pantalon", Categoria.PARTE_INFERIOR);
-    Material material = Material.JEAN;
-    ColorPrimario colorPrimario = new ColorPrimario(255,255,255);
-    ColorSecundario colorSecundario = new ColorSecundario(255,0,0);
+  void unaPrendaConColorSecundario() {
+    ColorSecundario color = new ColorSecundario(255,0,0);
+    assertEquals(unPantalonDeJeanBlancoYRojo().getColorSecundario().getColorRed() , color.getColorRed());
+    assertEquals(unPantalonDeJeanBlancoYRojo().getColorSecundario().getColorGreen() , color.getColorGreen());
+    assertEquals(unPantalonDeJeanBlancoYRojo().getColorSecundario().getColorBlue() , color.getColorBlue());
   }
 
   @Test
-  void unaCamperaDeCueroNegro() {
-    TipoPrenda tipo = new TipoPrenda("Pantalon", Categoria.PARTE_SUPERIOR);
-    Material material = Material.CUERO;
-    ColorPrimario colorPrimario = new ColorPrimario(0,0,0);
+  void laCategoriaDeUnaCamperaEsParteSuperior() {
+    assertEquals(unaCamperaDeCueroNegro().getCategoria().toString(), "PARTE_SUPERIOR");
+  }
+
+
+  private Prenda unaRemeraDeAlgodonAzul(){
+    return new Prenda(
+        new TipoPrenda("Remera", Categoria.PARTE_SUPERIOR),
+        Material.ALGODON,
+        new ColorPrimario(0,0,255));
+  }
+
+  private Prenda unPantalonDeJeanBlancoYRojo(){
+    return new Prenda(
+        new TipoPrenda("Pantalon", Categoria.PARTE_INFERIOR),
+        Material.JEAN,
+        new ColorPrimario(255,255,255),
+        new ColorSecundario(255,0,0));
+  }
+
+  private Prenda unaCamperaDeCueroNegro(){
+    return new Prenda(
+        new TipoPrenda("Pantalon", Categoria.PARTE_SUPERIOR),
+        Material.CUERO,
+        new ColorPrimario(0,0,0));
   }
 
 }
