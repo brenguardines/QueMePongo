985acbb818622f29f62d987b7b09093d7f90af6b
Brenda Guardines <82037465+brenguardines@users.noreply.github.com>
2023-05-23 15:13:13 -0300

    modificacion en nombres y agregado de getters

---
+++ src/ColorPrimario.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class ColorPrimario {
+  private Integer colorRed;
+  private Integer colorGreen;
+  private Integer colorBlue;
+
+  public ColorPrimario(Integer colorRed, Integer colorGreen, Integer colorBlue) {
+    this.colorRed = colorRed;
+    this.colorGreen = colorGreen;
+    this.colorBlue = colorBlue;
+  }
+}
---
+++ src/ColorSecundario.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class ColorSecundario {
+  private Integer colorRed;
+  private Integer colorGreen;
+  private Integer colorBlue;
+
+  public ColorSecundario(Integer colorRed, Integer colorGreen, Integer colorBlue) {
+    this.colorRed = colorRed;
+    this.colorGreen = colorGreen;
+    this.colorBlue = colorBlue;
+  }
+
+  public Integer getColorRed() {
+    return colorRed;
+  }
+
+  public Integer getColorGreen() {
+    return colorGreen;
+  }
+
+  public Integer getColorBlue() {
+    return colorBlue;
+  }
+}
